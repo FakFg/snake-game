a40e87330cfa9790f807e303641338f99700ae9f
FakFg <fake.fg@hotmail.com>
2016-12-14 11:59:35 -0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -25,11 +25,8 @@ function Snake() {
         this.first = false;
       }
     } else {
-      if ((this.y % 2) == 0) and
-         (this.x < width) {
-        this.dir(1, 0);
-      } else {
-        this.dir(0, 1);
+      if ((this.y % 2) == 0) {
+        this.ydestiny(30);
       }
     }
   }
