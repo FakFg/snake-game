0f9d313ddb0b60562eaa79538af2a6f3b5032cc5
FakFg <fake.fg@hotmail.com>
2016-12-14 10:45:05 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,17 @@
 function Bot() {
   this.cols = 0;
   this.rows = 0;
+  this.s;
   
-  function Bot(cols, rows) {
+  function Bot(cols, rows, s) {
     this.cols = cols;
     this.rows = rows;
+    this.s = s;
   }
   
   function setup() {
+    while(this.s.x > 0) {
+      this.s.dir(-1, 0);
+    }
   }
 }
