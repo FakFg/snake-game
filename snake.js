66030b1f059ed10bcec8e8cd44033b3d71801625
FakFg <fake.fg@hotmail.com>
2016-12-14 11:16:09 -0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -64,8 +64,6 @@ function Snake(x, y) {
   }
 
   this.show = function() {
-    console.log(this.x);
-    console.log(this.y);
     fill(255);
     for (var i = 0; i < this.tail.length; i++) {
       rect(this.tail[i].x, this.tail[i].y, scl, scl);
